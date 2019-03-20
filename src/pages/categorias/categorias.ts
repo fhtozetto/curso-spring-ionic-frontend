53c3a46d923b9c3bdecbece6ade64b5327bbb1cd
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2019-03-19 21:43:56 -0300

    Mostrando as categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,3 +1,5 @@
+import { API_CONFIG } from './../../Config/api.config';
+import { CategoriaDTO } from './../../models/categoria.dto';
 import { CategoriasService } from './../../services/domain/categoria.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -10,6 +12,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -17,7 +23,13 @@ export class CategoriasPage {
   }
 
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(response => {console.log(response)},error => {console.log(error)});
+    this.categoriaService.findAll()
+    .subscribe(response => {
+      this.items = response;
+    },
+    error => {
+      console.log(error)
+    });
     
   }
 
